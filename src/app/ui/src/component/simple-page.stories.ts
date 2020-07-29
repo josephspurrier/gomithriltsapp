75c96e5c10c14760814e5c9eafe8c69764c3e883
Joseph Spurrier <github@josephspurrier.com>
2020-07-28 23:26:19 -0400

    Fix stories

--- src/app/ui/src/component/simple-page.stories.ts
+++ src/app/ui/src/component/simple-page.stories.ts
@@ -11,12 +11,12 @@ export default {
 
 export const WithContent = (): m.Component => ({
   view: () =>
-    m(SimplePage, {
-      title: text("Title", "This is the Title"),
-      description: m(
-        "span",
-        text("Description", "This is a subtitle or description.")
-      ),
-      content: text("Content", "This is the content."),
-    }),
+    m(
+      SimplePage,
+      {
+        title: text("Title", "This is the Title"),
+        description: text("Description", "This is a subtitle or description."),
+      },
+      [text("Content", "This is the content.")]
+    ),
 });
--- src/app/ui/src/layout/main.stories.ts
+++ src/app/ui/src/layout/main.stories.ts
@@ -14,14 +14,17 @@ export const simplePage = (): m.Component => ({
   view: () => {
     return m(
       LayoutMain,
-      m(SimplePage, {
-        title: text("Title", "This is the Title"),
-        description: m(
-          "span",
-          text("Description", "This is a subtitle or description.")
-        ),
-        content: text("Content", "This is the content."),
-      })
+      m(
+        SimplePage,
+        {
+          title: text("Title", "This is the Title"),
+          description: text(
+            "Description",
+            "This is a subtitle or description."
+          ),
+        },
+        [text("Content", "This is the content.")]
+      )
     );
   },
 });
